165a264eb74ea5bee3aa33abb6ff4d0c6931e4e2
NanjoMiyako2 <140793758+NanjoMiyako2@users.noreply.github.com>
2024-12-31 15:18:06 +0900

    Add files via upload

--- iraisha.js
+++ iraisha.js
@@ -16,3 +16,29 @@ function calcTotalPrice(){
 	
 	return totalPrice1
 }
+
+function OutputStr(){
+
+	total1 = calcTotalPrice();
+	
+	outStr = "";
+	
+	for(i=0; i<20; i++){
+		priceIdStr = "price" + (i+1).toString();
+		nameStr = "buppin"+(i+1).toString();
+		
+		price1 = document.getElementById(priceIdStr);
+		name1 = document.getElementById(nameStr);
+
+		if(Number(price1.value) > 0){
+			outStr += "品物"+(i+1).toString()+":"+name1.value+":"+Number(price1.value)+"円\r\n"
+		}
+	}
+	
+	outStr += "\r\n合計金額:" + total1.toString() + "円\r\n";
+	
+	textArea1 = document.getElementById("TextArea1");
+	textArea1.value=outStr;
+	
+	
+}
